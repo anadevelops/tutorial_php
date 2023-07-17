04ffd42368f05799bf5461fa797e86984fbde692
Ana Clara <ana.stupp02@gmail.com>
2023-07-17 16:50:12 -0300

    Criando e aplicando variáveis

--- site.php
+++ site.php
@@ -12,6 +12,7 @@
         $characterAge = 35; 
         echo "There oce was a man named $characterName <br>";
         echo "He was $characterAge years old <br>";
+        $characterName = "Mike";
         echo "He really liked the name $characterName <br>";
         echo "But didn't like being $characterAge <br>";
 
