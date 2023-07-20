da26116841646204f890c1d66a798ce94c81f928
Ana Clara <ana.stupp02@gmail.com>
2023-07-20 16:14:13 -0300

    Uso de números e funções matemáticas

--- site.php
+++ site.php
@@ -8,7 +8,42 @@
 <body>
     
     <?php 
-       
+       //Utilizando números e operações;
+        echo 42 * 17 ;
+        echo "<br>";
+        echo 10 % 3;
+        echo "<br>";
+        echo 4 + 5 * 10;
+        echo "<br>";
+
+        //Função ++, --, -= e etc;
+        $num = 10;
+        $num++;
+        echo $num;
+        echo "<br>";
+        $num += 25;
+        echo $num;
+        echo "<br>";
+
+        //Funções matemáticas básicas;
+        echo abs(-100);
+        echo "<br>";
+        echo pow(2, 4);
+        echo "<br>";
+        echo sqrt(144);
+        echo "<br>";
+        echo max(2, 10);
+        echo "<br>";
+        echo min(2, 10);
+        echo "<br>";
+        echo round(3.2);
+        echo "<br>";
+        echo round(3.7);
+        echo "<br>";
+        echo ceil(3.3);
+        echo "<br>";
+        echo floor(3.3);
+
     ?>
 
 </body>
