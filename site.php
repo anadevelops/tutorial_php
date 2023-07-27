49a0b099a60976d021c7682bf33d55d7c0f63b1a
Ana Clara <ana.stupp02@gmail.com>
2023-07-27 16:15:58 -0300

    Uso de arrays

--- site.php
+++ site.php
@@ -6,9 +6,14 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- Uso de arrays em PHP !-->
     
     <?php 
-       
+        $friends = array("Maria", "Bianca", "Leticia");
+        $friends[1] = "Camila";
+        $friends[4] = "Bianca";
+        echo $friends[1];
+        echo count($friends);
     ?>
 
 </body>
