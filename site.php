269c478e9a1d4cfc1cb7026f4a7af7ed8c32fb47
Ana Clara <ana.stupp02@gmail.com>
2023-07-27 17:24:52 -0300

    Checkboxes com PHP

--- site.php
+++ site.php
@@ -6,9 +6,17 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- Uso de checkbox com PHP !-->
+    <form action="site.php" method="post">
+        Apples: <input type="checkbox" name="fruits[]" value="apples"> <br>
+        Oranges: <input type="checkbox" name="fruits[]" value="oranges"> <br>
+        Grapes: <input type="checkbox" name="fruits[]" value="grapes"> <br>
+        <input type="submit">
+    </form>
     
     <?php 
-       
+       $fruits = $_POST["fruits"];
+       echo $fruits [0];
     ?>
 
 </body>
