9994571aebf52d0d7e80e4a421cd1498bf36ff00
Ana Clara <ana.stupp02@gmail.com>
2023-07-27 16:00:28 -0300

    Diferenças entre POST e GET

--- site.php
+++ site.php
@@ -6,9 +6,30 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- Diferença entre POST e GET em PHP !-->
+    <!-- GET: Senha aparece na URL !-->
     
-    <?php 
-       
+    <form action="site.php" method="get">
+        Password: <input type="password" name="password"> <br>
+        <input type="submit">    
+    </form>
+    <br> <br>
+    
+
+    <?php  
+       echo $_GET["password"];
+    ?>
+
+    <!-- POST: mais seguro, senha não aparece na URL !-->
+    <form action="site.php" method="post">
+        Password: <input type="password" name="password"> <br>
+        <input type="submit">    
+    </form>
+    <br> <br>
+
+
+    <?php  
+       echo $_POST["password"];
     ?>
 
 </body>
